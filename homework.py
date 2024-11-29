bf38fb319e682ed4e4d15584b70cdf9d237ffe74
Asylbek Akunov <lovkach2006@gmail.com>
2024-11-30 02:23:00 +0600

    correction mistakes

--- homework.py
+++ homework.py
@@ -2,7 +2,7 @@ import random
 locations = [random.randint(1,7) for i in range (3)]
 weight = [200, 213, 300]
 print('Enter three locations:')
-while true:
+while True:
     attemps = [int(input()) for i in range (3)]
     if attemps == locations:
         totweight = sum(weight)
